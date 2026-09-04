c544bbbac30bc485d2f393ea0bf4e2d6e20d94dd
Murilo Pereira Bedim <Murilobedim@gmail.com>
2026-09-04 16:23:56 -0300

    feat: modela contrato de dados do Pokemon com interface (aula 2)

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,16 @@ const POKEMON_PER_PAGE = 20;
 const MAX_POKEMON_ID = 151;
 let currentPage = 1;
 let isDarkMode = false;
-console.log(`[Configuração Pokédex] API: ${API_URL} | Página: ${currentPage}`);
+const bulbasaur = {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["Grass", "Poison"],
+    hp: 45,
+    attack: 49,
+    spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+};
+console.log(bulbasaur.name.toUpperCase());
+console.log(bulbasaur.types);
+console.log(bulbasaur.attack);
+console.log(bulbasaur.spriteUrl);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,25 @@ const MAX_POKEMON_ID: number = 151;
 let currentPage: number = 1;
 let isDarkMode: boolean = false;
 
-console.log(`[Configuração Pokédex] API: ${API_URL} | Página: ${currentPage}`)
+interface Pokemon {
+    id: number;
+    name: string;
+    types: string[];
+    hp: number;
+    attack: number;
+    spriteUrl: string;
+}
+
+const bulbasaur: Pokemon = {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["Grass", "Poison"],
+    hp: 45,
+    attack: 49,
+    spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+}
+
+console.log(bulbasaur.name.toUpperCase());
+console.log(bulbasaur.types);
+console.log(bulbasaur.attack);
+console.log(bulbasaur.spriteUrl);
