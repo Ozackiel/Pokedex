01239abdbbf98cebd174ac770c4b77aa3fa07bb2
Murilo Pereira Bedim <Murilobedim@gmail.com>
2026-09-04 16:40:37 -0300

    feat: implementa formatacao, filtros e consumo assincrono da PokeAPI

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,34 @@ const POKEMON_PER_PAGE = 20;
 const MAX_POKEMON_ID = 151;
 let currentPage = 1;
 let isDarkMode = false;
-const bulbasaur = {
-    id: 1,
-    name: "Bulbasaur",
-    types: ["Grass", "Poison"],
-    hp: 45,
-    attack: 49,
-    spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-};
-console.log(bulbasaur.name.toUpperCase());
-console.log(bulbasaur.types);
-console.log(bulbasaur.attack);
-console.log(bulbasaur.spriteUrl);
+function formatPokemonId(id) {
+    return `#${id.toString().padStart(4, "0")}`;
+}
+function filterPokemonByType(pokemonList, types) {
+    if (types.length === 0) {
+        return pokemonList;
+    }
+    return pokemonList.filter((pokemon) => types.some((type) => pokemon.types.includes(type)));
+}
+async function fetchPokemon(id) {
+    const response = await fetch(`${API_URL}/pokemon/${id}`);
+    const data = await response.json();
+    return {
+        id: data.id,
+        name: data.name,
+        types: data.types.map((type) => type.type.name),
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        spriteUrl: data.sprites.front_default,
+    };
+}
+// Chamando a função assíncrona para buscar o Pokémon #1 (Bulbasaur) na internet
+fetchPokemon(40).then((pokemon) => {
+    console.log("Pokémon baixado com sucesso da PokéAPI:");
+    console.log(`Nome: ${pokemon.name.toUpperCase()}`);
+    console.log(`ID Formatado: ${formatPokemonId(pokemon.id)}`);
+    console.log(`Tipos: ${pokemon.types.join(", ")}`);
+    console.log(`HP: ${pokemon.hp} | Ataque: ${pokemon.attack}`);
+    console.log(`Foto: ${pokemon.spriteUrl}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,36 @@ interface Pokemon {
     spriteUrl: string;
 }
 
-const bulbasaur: Pokemon = {
-    id: 1,
-    name: "Bulbasaur",
-    types: ["Grass", "Poison"],
-    hp: 45,
-    attack: 49,
-    spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+function formatPokemonId(id: number): string {
+    return `#${id.toString().padStart(4, "0")}`;
 }
 
-console.log(bulbasaur.name.toUpperCase());
-console.log(bulbasaur.types);
-console.log(bulbasaur.attack);
-console.log(bulbasaur.spriteUrl);
+function filterPokemonByType(pokemonList: Pokemon[], types: string[]): Pokemon[] {
+    if (types.length === 0) {
+        return pokemonList;
+    }
+    return pokemonList.filter((pokemon) => types.some((type) => pokemon.types.includes(type)));
+}
+
+async function fetchPokemon(id: number): Promise<Pokemon> {
+    const response = await fetch(`${API_URL}/pokemon/${id}`);
+    const data = await response.json();
+    return {
+        id: data.id,
+        name: data.name,
+        types: data.types.map((type: any) => type.type.name),
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        spriteUrl: data.sprites.front_default,
+    };
+}
+
+// Chamando a função assíncrona para buscar o Pokémon #1 (Bulbasaur) na internet
+fetchPokemon(40).then((pokemon) => {
+    console.log("Pokémon baixado com sucesso da PokéAPI:");
+    console.log(`Nome: ${pokemon.name.toUpperCase()}`);
+    console.log(`ID Formatado: ${formatPokemonId(pokemon.id)}`);
+    console.log(`Tipos: ${pokemon.types.join(", ")}`);
+    console.log(`HP: ${pokemon.hp} | Ataque: ${pokemon.attack}`);
+    console.log(`Foto: ${pokemon.spriteUrl}`);
+});
